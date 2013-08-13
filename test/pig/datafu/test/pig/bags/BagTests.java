c5fad98647b6d9f3affef88258c165b07efca62e
William Vaughan <wvaughan@linkedin.com>
2013-08-12 17:59:16 -0700

    Add BagGroup UDF and tests

--- test/pig/datafu/test/pig/bags/BagTests.java
+++ test/pig/datafu/test/pig/bags/BagTests.java
@@ -1066,4 +1066,72 @@ public class BagTests extends PigTests
     test.runScript();
     assertOutput(test, "data2", "({(1,A),(1,B),(2,A),(2,B),(2,C),(3,A)})");
   }
+ 
+  /**
+  register $JAR_PATH
+
+  define BagGroup datafu.pig.bags.BagGroup();
+  
+  data = LOAD 'input' AS (input_bag: bag {T: tuple(k: int, v: chararray)});
+  describe data;
+  
+  data2 = FOREACH data GENERATE BagGroup(input_bag, input_bag.k) as grouped;
+  describe data2;
+  
+  data3 = FOREACH data2 {
+    ordered = ORDER grouped BY group;
+    GENERATE
+      ordered as grouped;
+  }
+  describe data3;
+  
+  STORE data INTO 'output';
+
+   */
+  @Multiline
+  private String bagGroupSingleTest;
+  
+  @Test
+  public void bagGroupSingleTest() throws Exception
+  {
+    PigTest test = createPigTestFromString(bagGroupSingleTest);
+    writeLinesToFile("input", "({(1,A),(1,B),(2,A),(2,B),(2,C),(3,A)})");
+    test.runScript();
+    getLinesForAlias(test, "data2", true);
+    assertOutput(test, "data3", "({(1,{(1,A),(1,B)}),(2,{(2,A),(2,B),(2,C)}),(3,{(3,A)})})");
+  }
+  
+  /**
+  register $JAR_PATH
+
+  define BagGroup datafu.pig.bags.BagGroup();
+  
+  data = LOAD 'input' AS (input_bag: bag {T: tuple(k: int, k2: chararray, v: int)});
+  describe data;
+  
+  data2 = FOREACH data GENERATE BagGroup(input_bag, input_bag.(k, k2)) as grouped;
+  describe data2;
+  
+  data3 = FOREACH data2 {
+    ordered = ORDER grouped BY group;
+    GENERATE
+      ordered as grouped;
+  }
+  describe data3;
+  
+  STORE data INTO 'output';
+
+   */
+  @Multiline
+  private String bagGroupMultipleTest;
+  
+  @Test
+  public void bagGroupMultipleTest() throws Exception
+  {
+    PigTest test = createPigTestFromString(bagGroupMultipleTest);
+    writeLinesToFile("input", "({(1,A,1),(1,B,1),(1,A,2),(2,A,1),(2,B,1),(2,C,1),(3,A,1)})");
+    test.runScript();
+    getLinesForAlias(test, "data2", true);
+    assertOutput(test, "data3", "({((1,A),{(1,A,1),(1,A,2)}),((1,B),{(1,B,1)}),((2,A),{(2,A,1)}),((2,B),{(2,B,1)}),((2,C),{(2,C,1)}),((3,A),{(3,A,1)})})");
+  }
 }

07c8ea3e9e1751609a80b576f98ace94277bbf56
Jarek Jarcec Cecho <jarcec@apache.org>
2013-10-23 12:21:25 -0700

    StreamingQuantileTests are failing when running against Pig 0.12.0

--- src/java/datafu/pig/stats/StreamingQuantile.java
+++ src/java/datafu/pig/stats/StreamingQuantile.java
@@ -26,7 +26,9 @@ import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 
 import com.google.common.collect.Lists;
 
@@ -259,7 +261,12 @@ public class StreamingQuantile extends AccumulatorEvalFunc<Tuple>
       for (Double x : this.quantiles)
         tupleSchema.add(new Schema.FieldSchema("quantile_" + x.toString().replace(".", "_"), DataType.DOUBLE));
     }
-    return tupleSchema;
+
+    try {
+      return new Schema(new FieldSchema(null, tupleSchema, DataType.TUPLE));
+    } catch(FrontendException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   static class QuantileEstimator

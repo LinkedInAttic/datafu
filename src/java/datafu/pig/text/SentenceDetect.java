4c76628a725f14be78929d8b2400b528f40be9b3
Russell Jurney <russell.jurney@gmail.com>
2014-01-04 15:40:20 -0800

    Sentence detect implemented.

---
+++ src/java/datafu/pig/text/SentenceDetect.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package datafu.pig.text;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import org.apache.pig.data.*;
+
+import datafu.pig.util.SimpleEvalFunc;
+
+/**
+ * The OpenNLP Tokenizers segment an input character sequence into tokens.
+ * <p>
+ * Example:
+ * <pre>
+ * {@code
+ * define Tokenize datafu.pig.bags.Tokenize();
+ *
+ * -- input:
+ * -- ("I believe the Masons have infiltrated the Apache PMC. I believe laser beams control cat brains.")
+ * infoo = LOAD 'input' AS (text:chararray);
+
+ * -- output:
+ * -- ({(I believe the Masons have infiltrated the Apache PMC.)(I believe laser beams control cat brains.)})
+ * outfoo = FOREACH input GENERATE SentenceDetect(text) as tokens;
+ * }
+ * </pre>
+ */
+public class SentenceDetect extends SimpleEvalFunc<DataBag>
+{
+    private static boolean isFirst = true;
+    InputStream is = null;
+    SentenceModel model = null;
+    SentenceDetectorME sdetector = null;
+    TupleFactory tf = TupleFactory.getInstance();
+    BagFactory bf = BagFactory.getInstance();
+
+    public DataBag call(String inputString) throws IOException
+    {
+        DataBag outBag = bf.newDefaultBag();
+        if(isFirst == true) {
+            is = new FileInputStream("data/en-sent.bin");
+            model = new SentenceModel(is);
+            sdetector = new SentenceDetectorME(model);
+
+            isFirst = false;
+        }
+        String sentences[] = sdetector.sentDetect(inputString);
+        for(String sentence : sentences) {
+            Tuple outTuple = tf.newTuple(sentence);
+            outBag.add(outTuple);
+        }
+        return outBag;
+    }
+}
--- test.pig
+++ test.pig
@@ -3,6 +3,12 @@ register lib/packaged/opennlp-maxent-bundle-3.0.3.jar
 register lib/packaged/opennlp-tools-bundle-1.5.3.jar
 register lib/packaged/opennlp-uima-jar-1.5.3.jar
 
-foo = LOAD '/Users/rjurney/Software/varaha/scripts/pos_tagging/data/ten.avro/' USING AvroStorage();
-out = foreach foo GENERATE datafu.pig.text.Tokenize(text) as tokens;
-dump out
+foo = LOAD 'data/ten.avro/' USING AvroStorage();
+out = FOREACH foo GENERATE datafu.pig.text.Tokenize(text) AS tokens;
+-- DUMP out
+
+out2 = FOREACH foo GENERATE datafu.pig.text.SentenceDetect(text) AS sentences;
+-- DUMP out2
+
+out3 = FOREACH foo GENERATE datafu.pig.text.Tokenize(datafu.pig.text.SentenceDetect(text)) AS tokens;
+DUMP out3
